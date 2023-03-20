c6ff53db40a0db2a115ba53a67341e770b9d31b9
OwLu0905 <owlu0905@gmail.com>
2023-03-21 01:20:01 +0800

    Initial out subtree commit gh-pages

---
+++ out/_next/static/xWqRDinod2aJDCUJkZsaJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-fb3f3c148c859656.js","static/chunks/pages/index-780d3ce806c2f1af.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
