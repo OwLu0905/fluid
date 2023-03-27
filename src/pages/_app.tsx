7f00ba2ba2863f3ab10076e2e836f9d336ae4480
OwLu0905 <owlu0905@gmail.com>
2023-03-27 23:57:08 +0800

    add dynamic title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,8 +24,14 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
             ? `第28屆全國計算流體力學學術研討會-${Component.displayName}`
             : "第28屆全國計算流體力學學術研討會"}
         </title>
+
+        // <title>NCFD 2023</title>
         */}
-        <title>NCFD 2023</title>
+            <title>
+          {Component.displayName
+            ? `${Component.displayName}`
+            : "NCFD 2023"}
+            </title>
         <meta name="description" content="第28屆全國計算流體力學學術研討會" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content="流體力學, 研討會, 28屆, 計算流體力學" />
