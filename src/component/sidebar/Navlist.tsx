64d9fcc51cf9c274089f7d2854618a52d0d6f1ac
OwLu0905 <owlu0905@gmail.com>
2023-06-09 20:25:36 +0800

    add award page

--- src/component/sidebar/Navlist.tsx
+++ src/component/sidebar/Navlist.tsx
@@ -11,12 +11,13 @@ const menuls = [
   { id: 4, name: "大會演講", href: "/speech" },
   { id: 5, name: "論壇發表", href: "/presentation" },
   { id: 6, name: "註冊與報名", href: "/register" },
-  { id: 7, name: "海報論文競賽", href: "/poster" },
-  { id: 8, name: "流體力學之美展覽", href: "/exhibition" },
-  { id: 9, name: "委員會", href: "/committee" },
-  { id: 10, name: "住宿資訊", href: "/accommodation" },
-  { id: 11, name: "聯絡資訊", href: "/contact" },
-  { id: 12, name: "交通資訊", href: "/traffic" },
+  { id: 7, name: "流體力學得獎名單", href: "/award" },
+  { id: 8, name: "海報論文競賽", href: "/poster" },
+  { id: 9, name: "流體力學之美展覽", href: "/exhibition" },
+  { id: 10, name: "委員會", href: "/committee" },
+  { id: 11, name: "住宿資訊", href: "/accommodation" },
+  { id: 12, name: "聯絡資訊", href: "/contact" },
+  { id: 13, name: "交通資訊", href: "/traffic" },
 ];
 // <li className="peer/home hover:border-l-2 px-2 py-2 hover:border-amber-600 hover:text-gray-300 hover:bg-sky-400/10">
 const Navlist = () => {
@@ -34,14 +35,16 @@ const Navlist = () => {
         return (
           <li
             key={item.name}
-            className={`rounded-lg font-semibold hover:bg-gray-300/20 sm:text-lg md:text-xl xl:text-2xl ${navActiveLink(item.href)
+            className={`rounded-lg font-semibold hover:bg-gray-300/20 sm:text-lg md:text-xl xl:text-2xl ${
+              navActiveLink(item.href)
                 ? "rounded-lg bg-gray-500/20 font-semibold text-sky-300 transition-all duration-300 ease-in-out hover:border-none hover:bg-gray-100/40 active:bg-gray-300/60"
                 : "text-sky-400/80"
-              }`}
+            }`}
           >
             <Link
-              className={`block w-full px-4 py-2  ${navActiveLink(item.href) ? "px-8 py-2" : ""
-                }`}
+              className={`block w-full px-4 py-2  ${
+                navActiveLink(item.href) ? "px-8 py-2" : ""
+              }`}
               href={item.href}
               scroll={false}
             >
---
+++ src/pages/award/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import AppLayout from "@/component/layouts/AppLayout";
+import NextSEO from "@/component/layouts/NextSEO";
+import Title from "@/component/utils/Title";
+
+const Award = () => {
+  return (
+    <>
+      <NextSEO
+        title="流力學會得獎名單"
+        description="第28屆全國計算流體力學學術研討會流力學會得獎名單"
+      />
+
+      <section
+        className="mx-auto block w-full text-gray-300/80"
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
+      >
+        <h3 className="text-2xl font-medium">建置中</h3>
+      </section>
+    </>
+  );
+};
+
+Award.Layout = AppLayout;
+Award.displayName = "流力學會得獎名單";
+
+export default Award;
