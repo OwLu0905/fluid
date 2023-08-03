2ad6de6b6caca05325bd5da29693693f016e0ab9
ko@KoChiJyunde-MBP <ws612531@gmail.com>
2023-08-03 11:40:08 +0800

    revise text

--- src/pages/presentation/index.tsx
+++ src/pages/presentation/index.tsx
@@ -23,6 +23,12 @@ const Presentation = () => {
             <br></br>
             題目：計算流體力學應用於超然衝壓引擎分析的挑戰
           </ol>
+          <ol className="mb-6 list-inside list-decimal space-y-4 text-base sm:text-lg md:indent-2 md:text-xl xl:text-2xl">
+            <br></br>
+            講者：劉博文 （中科院航空所氣體動力組）
+            <br></br>
+            題目：彈艙聲場和飛彈點火震波議題之數值分析與應用
+          </ol>
           <Title as="h2">
             迷你論壇 II：國家太空中心 ＆ 國家高速網路與計算中心
           </Title>
